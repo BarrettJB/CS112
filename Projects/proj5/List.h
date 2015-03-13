2ce295395adcad52daa48993ffd35e237598fe73
Barrett Bryson <bryson.barrett@gmail.com>
2015-03-13 10:49:01 -0400

    Added equality and readFrom(istream)

---
+++ Projects/proj5/List.h
@@ -0,0 +1,44 @@
+/*
+ * List.h
+ *
+ *  Created on: Mar 10, 2015
+ *      Author: bb36
+ */
+
+#ifndef LIST_H_
+#define LIST_H_
+
+#include <stddef.h>
+#include <iostream>
+using namespace std;
+
+
+typedef double Item;
+
+class List {
+public:
+	List();
+	List(const List& original);
+	virtual ~List();
+	unsigned getSize() const { return mySize; };
+	Item getFirst() const;
+	Item getLast() const;
+	void append(Item it);
+	List& operator=(const List& original);
+	bool operator==(const List& l2) const;
+	void readFrom(istream& in);
+private:
+	struct Node {
+		Item myItem;
+		Node* myNext;
+		Node();
+		Node(Item item,Node* next);
+		virtual ~Node();
+	};
+	Node* myFirst;
+	Node* myLast;
+	unsigned mySize;
+	friend class ListTester;
+};
+
+#endif /* LIST_H_ */
---
+++ Projects/proj5/tester.cpp
@@ -0,0 +1,23 @@
+/* tester.cpp
+ * Joel Adams, for CS 112 at Calvin College.
+ */
+ 
+#include "ListTester.h"
+#include <fstream>
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+int main() {
+	ListTester lt;
+	lt.runTests();
+//	ifstream fin("test.txt");
+//	assert(fin.is_open());
+//	double input;
+//	fin >> input;
+//	while (fin != NULL) {
+//		cout << input << " " << flush;
+//		fin >> input;
+//	}
+
+}
